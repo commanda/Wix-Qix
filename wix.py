23c544b815f7d0157371534aa84cecd6db14d297
commanda <amanda.wixted@gmail.com>
2011-06-25 21:23:21 -0400

    lines now draw cleanly, cursor traces them out

--- wix.py
+++ wix.py
@@ -61,11 +61,46 @@ class BoundingRect(Layer):
         self.left.draw()
         self.right.draw()
         glPopMatrix()
+
+
+class Lines(Layer):
+    def __init__(self, start_point):
+        super(Lines, self).__init__()
+        # Collection of lines we're currently working on drawing
+        self.currentFinishedLines = []
+        self.color = 255,255,255,255
+        self.width = 3
+        self.currentLine = Line(start_point, start_point, self.color, self.width)
+    
+    
+    def end_line_at_point(self, point):
+        new_start = self.currentLine.end
+        self.currentFinishedLines.append(self.currentLine)
+        self.currentLine = Line(new_start, point, self.color, self.width)
         
+    
+    def add_point(self, point):
+        self.currentLine.end = point
+    
+    def draw(self):
+        glPushMatrix()
+        self.transform()
+        
+        self.currentLine.draw()
+        
+        for line in self.currentFinishedLines:
+            line.draw()
+        
+        glPopMatrix()
+
+
+
+
+
 
 class Cursor( Sprite ):
     
-    def __init__(self, image, boundingRect):
+    def __init__(self, image, boundingRect, lines):
         super(Cursor, self).__init__(image)
         
         
@@ -73,28 +108,66 @@ class Cursor( Sprite ):
     
         self.rate = fast_rate
         self.boundingRect = boundingRect
+        self.lines = lines
+        self.direction = 0
+        
+
         
     def move_up(self):
-        print self.position
+        new_direction = key.UP
         if self.position[Y] < self.boundingRect.rect.top:
-            self.position = self.position[X], self.position[Y]+self.rate 
-    
+            self.lines.add_point(self.position)
+            self.position = self.position[X], self.position[Y]+self.rate
+            
+        else:
+            self.lines.end_line_at_point(self.position)
+        if new_direction != self.direction:
+            self.lines.end_line_at_point(self.position)
+        
+        self.direction = new_direction            
+            
+            
     def move_down(self):
-        print self.position
+        new_direction = key.DOWN
         if self.position[Y] > self.boundingRect.rect.bottom:
+            self.lines.add_point(self.position)
             self.position = self.position[X], self.position[Y]-self.rate 
+            
+        else:
+            self.lines.end_line_at_point(self.position)
+        if new_direction != self.direction:
+            self.lines.end_line_at_point(self.position)
+        
+        self.direction = new_direction
+
 
     def move_left(self):
-        print self.position
+        new_direction = key.LEFT
         if self.position[X] > self.boundingRect.rect.left:
+            self.lines.add_point(self.position)
             self.position = self.position[X]-self.rate, self.position[Y]
+            
+        else:
+            self.lines.end_line_at_point(self.position)
+        if new_direction != self.direction:
+            self.lines.end_line_at_point(self.position)
+        
+        self.direction = new_direction
     
     def move_right(self):
-        print self.position
+        new_direction = key.RIGHT
         if self.position[X] < self.boundingRect.rect.right:
+            self.lines.add_point(self.position)
             self.position = self.position[X]+self.rate, self.position[Y]
-    
-
+                    
+        else:
+            self.lines.end_line_at_point(self.position)
+        
+        if new_direction != self.direction:
+            self.lines.end_line_at_point(self.position)
+        
+        self.direction = new_direction
+        
 
 
 class GameControl (Layer):
@@ -106,7 +179,8 @@ class GameControl (Layer):
         super(GameControl, self).__init__()
         self.cursor = cursor
         self.held_down_key = 0
-        print 'cursor: ' + str(cursor)
+        
+       
         
     def on_key_press(self, pressed, modifiers):
         if pressed == key.UP:
@@ -130,7 +204,6 @@ class GameControl (Layer):
             self.cursor.rate = no_rate
     
     def on_key_release(self, symbol, modifiers):
-        print 'key release: ' + str(symbol)
         if symbol in (key.X, key.Z):
             self.cursor.rate = no_rate
         
@@ -151,14 +224,21 @@ if __name__ == "__main__":
     director.init(resizable=True, width=800, height=800)
     scene = Scene()
     scene.add( BackgroundLayer(), z=0)
+
     
     rect = Rect(50, 50, 700, 700)
     boundingRect = BoundingRect(rect)
+
+    start_point = boundingRect.rect.midbottom
+
+    lines = Lines(start_point)
+    #lines.end_line_at_point(start_point)
+    scene.add(lines, z=0)
+    
     image = pyglet.resource.image('cursor.png')
-    cursor = Cursor(image, boundingRect)
+    cursor = Cursor(image, boundingRect, lines)
     scene.add( cursor, z=1)
     scene.add( GameControl(cursor) )
     scene.add(boundingRect, z=1)
     
     director.run(scene)
-        
